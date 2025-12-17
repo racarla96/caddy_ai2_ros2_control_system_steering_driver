14e26fc3a4beefaf5cbf8c5a8694489968011ca5
Rafael Carbonell Lázaro <racarla96@gmail.com>
2025-12-17 17:51:43 +0100

    Update system_steering.launch.py

--- bringup/launch/system_steering.launch.py
+++ bringup/launch/system_steering.launch.py
@@ -38,7 +38,7 @@ def generate_launch_description():
                 ]
             ),
             " ",
-            f"controller_manager_frequency_hz:={update_rate}",
+            f"update_rate:={update_rate}",
         ]
     )
 
